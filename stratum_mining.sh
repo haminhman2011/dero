95600681262909681aa3366fd005acfb8eb162f3
haminhman2011 <haminhman201193@gmail.com>
2023-02-23 07:45:07 +0700

    up

--- stratum_mining.sh
+++ stratum_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS to run the miner"
 while :; do
-    ./astrominer -w YOUR_ADDRESS -r stratum+tls://pool.whalesburg.com:4300 -p stratum;
+    ./astrominer -w deroi1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xq9pvfz92xnjlr6axqxf99t -r stratum+tls://pool.whalesburg.com:4300 -p stratum;
     sleep 5;
 done
