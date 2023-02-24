9f4a2b5809417f26cc771faaf4221af8203c0ab4
haminhman2011 <haminhman201193@gmail.com>
2023-02-24 09:00:42 +0700

    up

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w dero1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xqqm622zv -r community-pools.mysrv.cloud:10300 -r2 dero.rabidmining.com:10300 -p rpc; 
+    ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xc66h4hzkyauc3sapvscm -r community-pools.mysrv.cloud:10300 -p rpc;
     sleep 5;
 done
